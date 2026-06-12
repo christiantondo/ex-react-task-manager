873087b6e0b50bd469a6070d53ca330f43d9010f
Christian Tondo <christiantondo1@gmail.com>
2026-06-13 01:30:58 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+function App() {
+
+    return (
+        <>
+            <div>
+                <h1>Task Manager Avanzato</h1>
+            </div>
+        </>
+    )
+}
+
+export default App
